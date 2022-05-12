64241e7c1704642ac607f081960a030efe8c5833
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-05-12 16:13:53 -0500

    Creating list of user in studentController

--- src/main/java/com/practis/demo/Application.java
+++ src/main/java/com/practis/demo/Application.java
@@ -1,7 +1,15 @@
 package com.practis.demo;
 
+import com.practis.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -11,4 +19,7 @@ public class Application {
         System.out.println("Hello");
     }
 
+
 }
+
+//https://www.youtube.com/watch?v=9SGDpanrc8U
---
+++ src/main/java/com/practis/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.practis.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L,"Ulisses","diegoUlisses@gmail.com",
+                        LocalDate.of(2021, Month.APRIL,4),
+                        19
+                )
+        );
+    }
+}
